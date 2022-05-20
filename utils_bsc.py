ae452fa8ebbbce23eb6e91bf3d04933f2858d5c9
Sergio Tallo Torres <ic19b047@technikum-wien.at>
2022-05-20 15:37:17 +0200

    Minor Changes

--- utils_bsc.py
+++ utils_bsc.py
@@ -83,6 +83,7 @@ def daily_plot(data, col, name):
     ax1.boxplot(datafix)
     plt.show()
 
+
 # apply the mean / stddev scaling in Pandas using the .mean() and .std() methods
 def normalize_mean_std_dataset(df):
     # copy the dataframe
@@ -95,7 +96,6 @@ def normalize_mean_std_dataset(df):
 
 
 def create_sequece_dataloaders(dataset, seq_length, batch_size, device):
-
     # Create a dataset with pairs data / next /Target (in this case data is one
     # sequence of seq_length measures (18 features), next is the next value in the sequence
     # and target is the following value with the
@@ -107,10 +107,10 @@ def create_sequece_dataloaders(dataset, seq_length, batch_size, device):
 
     print('Creating train/test data loaders')
 
-    for i in tqdm(range(len(dataset) - (seq_length +1))):
-        data = np.array(dataset.iloc[i:i+seq_length, 1:])
-        next = np.array(dataset.iloc[i+seq_length, 1:], dtype= float)
-        target = np.array(dataset.iloc[i+(seq_length + 1), 1:], dtype= float)
+    for i in tqdm(range(len(dataset) - (seq_length + 1))):
+        data = np.array(dataset.iloc[i:i + seq_length, 1:])
+        next = np.array(dataset.iloc[i + seq_length, 1:], dtype=float)
+        target = np.array(dataset.iloc[i + (seq_length + 1), 1:], dtype=float)
 
         pair_set.append((data, next, target))
 
@@ -168,15 +168,14 @@ def create_sequece_dataloaders(dataset, seq_length, batch_size, device):
 
 
 def create_dataloaders(dataset, device):
-
     # Create a dataset with pairs data / Target (in this case data is one measure (18 features) and target is the next measure (18 features))
     # When you plug in one measure, the model should out the next measure
 
     pair_set = []
 
-    for i in tqdm(range(len(dataset) -1)):
+    for i in tqdm(range(len(dataset) - 1)):
         data = np.array([j for j in dataset.iloc[i, 1:]])
-        target = np.array([j for j in dataset.iloc[i+1, 1:]])
+        target = np.array([j for j in dataset.iloc[i + 1, 1:]])
 
         pair_set.append((data, target))
 
@@ -223,6 +222,7 @@ def create_dataloaders(dataset, device):
 
     return loader_train, loader_test
 
+
 class ANN_relu(nn.Module):
 
     def __init__(self, D_in, D_out):
@@ -250,9 +250,9 @@ class ANN_relu(nn.Module):
 
         return self.linear4(x)
 
-# This function trains the model for one epoch
-def train_FFN(model, criterion, optimizer, train_loader, test_loader, n_epochs, train_loss = None, test_loss = None):
 
+# This function trains the model for one epoch
+def train_FFN(model, criterion, optimizer, train_loader, test_loader, n_epochs, train_loss=None, test_loss=None):
     if train_loss is not None:
         epoch_loss_train = train_loss
         best_train_loss = min([np.mean(i) for i in train_loss])
@@ -274,14 +274,13 @@ def train_FFN(model, criterion, optimizer, train_loader, test_loader, n_epochs, 
     best_model = model
     starting_epoch = len(epoch_loss_test)
 
-    for e in range(1, n_epochs +1):
+    for e in range(1, n_epochs + 1):
         print(f'\nEpoch {e + starting_epoch}:')
 
         print('Train')
         model.train()
 
         for i in tqdm(train_loader):
-
             data, target = i[0], i[1]
 
             optimizer.zero_grad()
@@ -289,13 +288,13 @@ def train_FFN(model, criterion, optimizer, train_loader, test_loader, n_epochs, 
             # Forward Pass
             output = model(data)
 
-            #Compute loss
+            # Compute loss
             loss = criterion(output, target)
 
-            #Backpropagation
+            # Backpropagation
             loss.backward()
 
-            #Optimization
+            # Optimization
             optimizer.step()
 
         losses_train = []
@@ -304,12 +303,11 @@ def train_FFN(model, criterion, optimizer, train_loader, test_loader, n_epochs, 
         model.eval()
         with torch.no_grad():
             for i in tqdm(train_loader):
-
                 data, target = i[0], i[1]
 
                 output = model(data)
 
-                losses_train.append (float(criterion(output, target).item()))
+                losses_train.append(float(criterion(output, target).item()))
 
         print('\nCurrent Mean loss Train: ', np.mean(losses_train))
         epoch_loss_train.append(losses_train)
@@ -320,13 +318,11 @@ def train_FFN(model, criterion, optimizer, train_loader, test_loader, n_epochs, 
         model.eval()
         with torch.no_grad():
             for i in tqdm(test_loader):
-
                 data, target = i[0], i[1]
 
                 output = model(data)
 
-                losses_test.append (float(criterion(output, target).item()))
-
+                losses_test.append(float(criterion(output, target).item()))
 
         print('\nCurrent Mean loss: ', np.mean(losses_test))
         epoch_loss_test.append(losses_test)
@@ -339,9 +335,9 @@ def train_FFN(model, criterion, optimizer, train_loader, test_loader, n_epochs, 
 
     return (best_model, best_train_loss, best_test_loss, best_epoch), epoch_loss_train, epoch_loss_test
 
-def print_results_training(train_loss, test_loss, title, test_loss_baseline = None, baseline_label = None):
 
-    fig = plt.figure(figsize = (10,10))
+def print_results_training(train_loss, test_loss, title, test_loss_baseline=None, baseline_label=None):
+    fig = plt.figure(figsize=(10, 10))
 
     ax = fig.add_subplot(111)
     plt.ion()
@@ -351,13 +347,14 @@ def print_results_training(train_loss, test_loss, title, test_loss_baseline = No
 
     if test_loss_baseline is not None:
         ax.plot(test_loss_baseline, label=baseline_label)
-    ax.plot([np.mean(i) for i in train_loss], label= 'Train_loss')
-    ax.plot([np.mean(i) for i in test_loss], label= 'Test_loss')
+    ax.plot([np.mean(i) for i in train_loss], label='Train_loss')
+    ax.plot([np.mean(i) for i in test_loss], label='Test_loss')
     ax.set_title(title)
     ax.set_xlabel('Epoch')
     ax.set_ylabel('Mean Squared Error')
     ax.legend()
     fig.canvas.draw()
+
 
 def count_parameters(model: torch.nn.Module, only_trainable: bool = True) -> int:
     """
@@ -371,15 +368,21 @@ def count_parameters(model: torch.nn.Module, only_trainable: bool = True) -> int
     return sum(parameter.numel() for parameter in model.parameters() if any(
         (not only_trainable, only_trainable and parameter.requires_grad)))
 
+
 class Transformer(nn.Module):
-    def __init__(self, feature_size, output_size, num_encoder_layers, num_heads, num_decoder_layers, device, dim_feedforward: int=2048, dropout: float =0.1, batch_first: bool = False):
+    def __init__(self, feature_size, output_size, num_encoder_layers, num_heads, num_decoder_layers, device,
+                 dim_feedforward: int = 2048, dropout: float = 0.1, batch_first: bool = False):
         super(Transformer, self).__init__()
 
-        encoder_layer = nn.TransformerEncoderLayer(d_model= feature_size, nhead= num_heads, dim_feedforward=dim_feedforward, dropout=dropout, device=device, batch_first=batch_first)
-        decoder_layer = nn.TransformerDecoderLayer(d_model= feature_size, nhead= num_heads, dim_feedforward=dim_feedforward, dropout=dropout, device=device, batch_first=batch_first)
+        encoder_layer = nn.TransformerEncoderLayer(d_model=feature_size, nhead=num_heads,
+                                                   dim_feedforward=dim_feedforward, dropout=dropout, device=device,
+                                                   batch_first=batch_first)
+        decoder_layer = nn.TransformerDecoderLayer(d_model=feature_size, nhead=num_heads,
+                                                   dim_feedforward=dim_feedforward, dropout=dropout, device=device,
+                                                   batch_first=batch_first)
 
-        self.encoder = nn.TransformerEncoder(encoder_layer, num_layers= num_encoder_layers)
-        self.decoder = nn.TransformerDecoder(decoder_layer, num_layers= num_decoder_layers)
+        self.encoder = nn.TransformerEncoder(encoder_layer, num_layers=num_encoder_layers)
+        self.decoder = nn.TransformerDecoder(decoder_layer, num_layers=num_decoder_layers)
 
         self.output_size = output_size
         self.device = device
@@ -393,27 +396,30 @@ class Transformer(nn.Module):
 
         for pos in range(seq_len):
             for i in range(0, int(dim_model / 2)):
-                position_encoding[pos, 2 * i] = math.sin(pos / (10000 ** ((2 * i)/dim_model)))
-                position_encoding[pos, (2 * i) + 1] = math.cos(pos / (10000 ** ((2 * i)/dim_model)))
+                position_encoding[pos, 2 * i] = math.sin(pos / (10000 ** ((2 * i) / dim_model)))
+                position_encoding[pos, (2 * i) + 1] = math.cos(pos / (10000 ** ((2 * i) / dim_model)))
 
         position_encoding = position_encoding.to(device)
 
         return position_encoding
 
-    def forward (self, enc_input, dec_input):
+    def forward(self, enc_input, dec_input):
 
         memory_mask = self.generate_square_mask(len(enc_input))
 
-        src_pos_enc = enc_input + self.positional_encoding(seq_len= enc_input.shape[1], dim_model= enc_input.shape[2], device= self.device)
-        src_pos_dec = dec_input + self.positional_encoding(seq_len= dec_input.shape[1], dim_model= dec_input.shape[2], device= self.device)
+        src_pos_enc = enc_input + self.positional_encoding(seq_len=enc_input.shape[1], dim_model=enc_input.shape[2],
+                                                           device=self.device)
+        src_pos_dec = dec_input + self.positional_encoding(seq_len=dec_input.shape[1], dim_model=dec_input.shape[2],
+                                                           device=self.device)
 
-        output = self.encoder (src= src_pos_enc, mask=None)
-        output = self.decoder (tgt= src_pos_dec, memory= output, tgt_mask=None, memory_mask=None)
+        output = self.encoder(src=src_pos_enc, mask=None)
+        output = self.decoder(tgt=src_pos_dec, memory=output, tgt_mask=None, memory_mask=None)
 
         return output
 
-def training_transformer(model, optimizer, criterion, train_loader, test_loader, n_epochs, train_loss = None, test_loss = None):
 
+def training_transformer(model, optimizer, criterion, train_loader, test_loader, n_epochs, train_loss=None,
+                         test_loss=None):
     if train_loss is not None:
         epoch_loss_train = train_loss
         best_train_loss = min([np.mean(i) for i in train_loss])
@@ -442,42 +448,38 @@ def training_transformer(model, optimizer, criterion, train_loader, test_loader,
         model.train()
 
         for i in tqdm(train_loader):
-
             input = i[0]
-            out = i[1].unsqueeze(0).permute(1,0,2)
-            target = i[2].unsqueeze(0).permute(1,0,2)
+            out = i[1].unsqueeze(0).permute(1, 0, 2)
+            target = i[2].unsqueeze(0).permute(1, 0, 2)
 
             net_out = model.forward(input, out)
 
-            #Compute loss
+            # Compute loss
             loss = criterion(net_out, target)
 
             optimizer.zero_grad()
 
-            #Backpropagation
+            # Backpropagation
             loss.backward()
 
             torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
 
-            #Optimization
+            # Optimization
             optimizer.step()
-
 
         print('\nTest with training set')
         losses_train = []
         model.eval()
         with torch.no_grad():
             for i in tqdm(train_loader):
-
                 input = i[0]
-                out = i[1].unsqueeze(0).permute(1,0,2)
-                target = i[2].unsqueeze(0).permute(1,0,2)
+                out = i[1].unsqueeze(0).permute(1, 0, 2)
+                target = i[2].unsqueeze(0).permute(1, 0, 2)
 
                 net_out = model.forward(input, out)
 
-                #Compute loss
-                losses_train.append (float(criterion(net_out, target).item()))
-
+                # Compute loss
+                losses_train.append(float(criterion(net_out, target).item()))
 
         print('\nCurrent Mean loss Train Set: ', np.mean(losses_train))
         epoch_loss_train.append(losses_train)
@@ -486,18 +488,16 @@ def training_transformer(model, optimizer, criterion, train_loader, test_loader,
         losses_test = []
         model.eval()
 
-
         with torch.no_grad():
             for i in tqdm(test_loader):
-
                 input = i[0]
-                out = i[1].unsqueeze(0).permute(1,0,2)
-                target = i[2].unsqueeze(0).permute(1,0,2)
+                out = i[1].unsqueeze(0).permute(1, 0, 2)
+                target = i[2].unsqueeze(0).permute(1, 0, 2)
 
                 net_out = model.forward(input, out)
 
-                #Compute loss
-                losses_test.append (float(criterion(net_out, target).item()))
+                # Compute loss
+                losses_test.append(float(criterion(net_out, target).item()))
 
         print('\nCurrent Mean loss Test Set: ', np.mean(losses_test))
         epoch_loss_test.append(losses_test)
@@ -512,21 +512,23 @@ def training_transformer(model, optimizer, criterion, train_loader, test_loader,
 
     return (best_model, best_train_loss, best_test_loss, best_epoch), epoch_loss_train, epoch_loss_test
 
+
 def define_train_transformers(models, device, dataset, training_results_transformers, path_save, colab):
     for i in models:
         if models[i][7] is True:
 
-            loader_train, loader_test = create_sequece_dataloaders(dataset=dataset, seq_length=models[i][8], batch_size=models[i][9], device=device)
+            loader_train, loader_test = create_sequece_dataloaders(dataset=dataset, seq_length=models[i][8],
+                                                                   batch_size=models[i][9], device=device)
 
             # Initialize Transformer Model and Optimizer
-            model = Transformer (num_encoder_layers=models[i][0],
-                                           num_decoder_layers=models[i][1],
-                                           feature_size=18,
-                                           output_size=18,
-                                           num_heads=models[i][2],
-                                           dim_feedforward=models[i][3],
-                                           device = device,
-                                           batch_first=True)
+            model = Transformer(num_encoder_layers=models[i][0],
+                                num_decoder_layers=models[i][1],
+                                feature_size=18,
+                                output_size=18,
+                                num_heads=models[i][2],
+                                dim_feedforward=models[i][3],
+                                device=device,
+                                batch_first=True)
 
             print(f'Model: {type(model).__name__} - {i}')
             print(f'{count_parameters(model)} trainable parameters.')
@@ -548,13 +550,12 @@ def define_train_transformers(models, device, dataset, training_results_transfor
             start_time = datetime.now()
 
             best_results, train_losses, test_losses = training_transformer(
-                model= model,
-                optimizer= optimizer,
-                criterion= criterion,
-                train_loader= loader_train,
-                test_loader= loader_test,
-                n_epochs= n_epochs)
-
+                model=model,
+                optimizer=optimizer,
+                criterion=criterion,
+                train_loader=loader_train,
+                test_loader=loader_test,
+                n_epochs=n_epochs)
 
             Transformer_trained_Model = best_results[0]
             best_train_loss = best_results[1]
@@ -572,7 +573,8 @@ def define_train_transformers(models, device, dataset, training_results_transfor
 
             print(f'Training time: {execution_time} seconds')
 
-            training_results_transformers[i] = [Transformer_trained_Model, best_train_loss, best_test_loss, best_epoch_number, train_losses, test_losses, execution_time]
+            training_results_transformers[i] = [Transformer_trained_Model, best_train_loss, best_test_loss,
+                                                best_epoch_number, train_losses, test_losses, execution_time]
 
             # save to npy file
             np.save(path_save + '/Transformer_' + i + '_train.npy', train_losses)
